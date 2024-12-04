7a7a1f8f5d417e886cb454be4167f0fc10b958fa
Madenginer <anasabrahat11@gmial.com>
2024-12-04 14:45:06 +0700

    Portofolio

---
+++ script.js
@@ -0,0 +1,31 @@
+emailjs.init("k-ynC6ygh1ACVMw2V"); // ganti dengan Public Key Anda
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // Mencegah reload halaman
+  const formData = new FormData(e.target);
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
+  emailjs
+    .send("service_e55p6mk", "template_pmr35tx", { //ganti dengan service_id dan template_id
+      from_name: name,
+      reply_to: email,
+      message: message,
+    })
+    .then((response) => {
+      Swal.fire({
+        icon: "success",
+        title: "Pesan Terkirim!",
+        text: "Pesan Anda telah berhasil dikirim. Kami akan segera menghubungi Anda.",
+      });
+      e.target.reset(); // reset form setelah pengiriman
+    })
+    .catch((error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Gagal Mengirim Pesan",
+        text: "Silakan coba lagi nanti.",
+      });
+      console.error("Error:", error);
+    });
+});
